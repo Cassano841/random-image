452b74d360c7318d864a0a0d1d8e9846e9c3cc21
Cassano <ninocb@gmail.com>
2022-03-20 18:32:53 -0300

    Update inicial

---
+++ script.js
@@ -0,0 +1,25 @@
+const randomImage = document.querySelector("#random-image");
+
+var accessKeyUnsplash = "fSAYhKApDP1hl0lBUlNfOecS5hJHtVYPuTZfDuieDJY";
+
+const urlUnsplash = `https://api.unsplash.com/photos/random/?client_id=${accessKeyUnsplash}`;
+
+let output = "";
+
+
+const randomPhoto = (photo) => {
+  console.log(photo);
+  output += `
+  <div id="center">
+  <img src=${photo.urls.small}>
+  <p>Description: ${photo.description||"Sem Descrição"}</p>
+  <p>Bio: ${photo.user.bio||"Sem Bio"}</p>
+  <p>Go to profile: ${photo.user.links.html}</p>
+  </div>
+`;
+  randomImage.innerHTML += output;
+};
+
+fetch(urlUnsplash)
+  .then((res) => res.json())
+  .then((data) => randomPhoto(data));
